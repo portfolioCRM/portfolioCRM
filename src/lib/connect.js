ae00ed12493c90bb7ef637e8e4d70e8b7c950878
Raed-Nehos <raedrdhaounia@gmail.com>
2024-05-30 18:59:45 +0100

    test connection to database and get list of projects

---
+++ src/lib/connect.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+const {MongoDBdataBase, MongoDBUserName, MongoDBPassword} = process.env
+const uri  = `mongodb+srv://${MongoDBUserName}:${MongoDBPassword}@cluster0.ywnsq.mongodb.net/`;
+
+const client = new MongoClient(uri);
+
+async function main(_collection) {
+    try {
+        await client.connect();
+        const collection = client.db(MongoDBdataBase)?.collection(_collection);
+        return collection;
+    } catch (error) {
+        return null
+    }
+
+  }
+
+export default main;
--- src/lib/connect.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from 'mongodb'
-
-
-const uri = "mongodb+srv://raedrdhaounia:dadHH123@cluster0.ywnsq.mongodb.net/"
-
- const  client = new MongoClient(uri as string)
- export const connect = client.connect();
